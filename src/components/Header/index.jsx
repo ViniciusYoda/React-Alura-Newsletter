0be8f918e625b52cdbc1762f3b7049cb9d2f09a4
Vinícius Yoda <vyoda4604@gmail.com>
2023-01-31 14:53:36 -0300

    dark mode

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,11 @@
-import { MoonIcon } from "@heroicons/react/24/solid"
+import ToggleTheme from "../ToggleTheme/ToggleTheme"
 
 const Header = () => {
    return(
-      <div className="flex h-20 bg-gray-500 justify-between items-center px-5 sm:rounded-xl sm:m-5">
+      <div className="flex h-20 bg-alura-200 dark-bg-200 justify-between items-center px-5 sm:rounded-xl sm:m-5">
          <span className="text-gray-100">Olá, Usuário</span>
          <h1 className="text-gray-100 text-xl">Alura Newsletter</h1>
-         <MoonIcon className="h-8 text-gray-100 hidden sm:block cursor-pointer" />
+         <ToggleTheme />
       </div>
    )
 }
---
+++ src/components/ToggleTheme/ToggleTheme.jsx
@@ -0,0 +1,28 @@
+import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
+import { useEffect } from "react";
+
+const ToggleTheme = () => {
+   const systemPreference = window.matchMedia('(prefers-color-scheme: dark').matches
+   const pageClasses = document.document.classList
+
+   useEffect(() => {
+      systemPreference && pageClasses.add("dark")
+   })
+   const toggle = () => {
+      pageClasses.toggle('dark')
+   }
+   return(
+      <div className="hidden sm:block">
+         <MoonIcon 
+            className="h-8 text-gray-100 block dark:hidden cursor-pointer" 
+            onClick={toggle}
+         />
+         <SunIcon 
+            className="h-8 text-gray-100 hidden dark:block cursor-pointer" 
+            onClick={toggle}
+         />
+      </div>
+   )
+}
+
+export default ToggleTheme
