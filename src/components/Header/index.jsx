c3467942bbfff437e8d74680441d4f5128b0264a
Vinícius Yoda <vyoda4604@gmail.com>
2023-01-31 13:55:40 -0300

    icon

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,11 @@
+import { MoonIcon } from "@heroicons/react/24/solid"
+
 const Header = () => {
    return(
       <div className="flex h-20 bg-gray-500 justify-between items-center px-5 sm:rounded-xl sm:m-5">
          <span className="text-gray-100">Olá, Usuário</span>
          <h1 className="text-gray-100 text-xl">Alura Newsletter</h1>
-         <input className="hidden sm:block" type="checkbox" />
+         <MoonIcon className="h-8 text-gray-100 hidden sm:block cursor-pointer" />
       </div>
    )
 }
