9b39227d8ddeb4439e9de8e45759e1b613c65181
Vinícius Yoda <vyoda4604@gmail.com>
2023-02-01 11:16:31 -0300

    articles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
+import ArticleList from "./components/ArticleList";
 
 const App = () => {
   return(
-    <Header/>
+    <>
+      <Header/>
+      <ArticleList/>
+    </>
   )
 }
 
