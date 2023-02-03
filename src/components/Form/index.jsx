edc46bdd9b68cbc8cb7542bcc075ad8f9661df35
Vinícius Yoda <vyoda4604@gmail.com>
2023-02-03 20:17:22 -0300

    ajuste

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -7,10 +7,28 @@ const Form = ({ onSubmit }) => {
       onSubmit({name, email})
    }
    return(
-      <form onSubmit={safeSubmit} className="h-full flex flex-col items-center just-fy-center -mt-20 gap-10 mx-5">
-         <input type="text" className="alura-input" required placeholder="Insira seu nome..." />
-         <input type="email" className="alura-input" required placeholder="Insira seu e-mail..." />
-         <button type="submit" className="alura-button">Seguir</button>
+      <form 
+         onSubmit={safeSubmit} 
+         className="h-full flex flex-col items-center just-fy-center -mt-20 gap-10 mx-5"
+      >
+         <input 
+            type="text" 
+            className="alura-input" 
+            required 
+            placeholder="Insira seu nome..." 
+         />
+         <input 
+            type="email" 
+            className="alura-input" 
+            required 
+            placeholder="Insira seu e-mail..." 
+         />
+         <button 
+            type="submit" 
+            className="alura-button"
+         >
+            Seguir
+         </button>
       </form>
    )
 }
